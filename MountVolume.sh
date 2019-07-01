a2e00bc6a71420b365c05f8a8ee29f6345af4cf0
GPR-ZRH <45576857+GPR-ZRH@users.noreply.github.com>
2019-07-01 02:42:46 +0200

    Update MountVolume.sh

--- MountVolume.sh
+++ MountVolume.sh
@@ -4,10 +4,10 @@ printf "\e[31mPlease enter the name of your volume as presented on the digitaloc
 read volumeName
 
 # Create a mount point for your volume:
-mkdir -p /mnt/$volumeName
+mkdir -p "/mnt/$volumeName"
 
 # Mount your volume at the newly-created mount point:
-mount -o discard,defaults,noatime /dev/disk/by-id/scsi-0DO_Volume_$volumeName /mnt/$volumeName
+mount -o "discard,defaults,noatime /dev/disk/by-id/scsi-0DO_Volume_$volumeName /mnt/$volumeName"
 
 # Change fstab so the volume will be mounted after a reboot
 echo "/dev/disk/by-id/scsi-0DO_Volume_$volumeName /mnt/$volumeName ext4 defaults,nofail,discard 0 0" | sudo tee -a /etc/fstab
