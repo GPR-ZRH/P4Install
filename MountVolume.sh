903d89b059b55ba52012e4aa1d323ec4c273f59f
GPR-ZRH <45576857+GPR-ZRH@users.noreply.github.com>
2019-07-01 02:19:23 +0200

    Update MountVolume.sh

--- MountVolume.sh
+++ MountVolume.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-printf "\e[31mPlease enter the name of your volume as presented on the digitalocean website -> Droplets -> Volumes\e[39m"
+printf "\e[31mPlease enter the name of your volume as presented on the digitalocean website -> Droplets -> Volumes\e[39m\n"
 read volumeName
 
 # Create a mount point for your volume:
