16dcce479b30568b0fe2d3628b666d562b3ac332
Michael Meier <michaelmeier@uexilon.ch>
2019-07-01 03:37:05 +0200

    still dealing with line endings...

--- MountVolume.sh
+++ MountVolume.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 printf "\e[31mPlease enter the name of your volume as presented on the digitalocean website -> Droplets -> Volumes\e[39m\n"
-
+printf "\e[31mDO NOT COPY! Write it.. copy and paste may introduce errors if line endings get copied too.\e[39m\n"
 volumeName=""
 
 read volumeName
 
-#directory="/mnt/$volumeName"
-volumeName="${volumeName%%'*}"
 printf "Create a mount point for your volume at /mnt/$volumeName\n"
 mkdir -p "/mnt/$volumeName"
 
@@ -18,4 +16,3 @@ mount -o discard,defaults,noatime "/dev/disk/by-id/scsi-0DO_Volume_$volumeName" 
 echo "/dev/disk/by-id/scsi-0DO_Volume_$volumeName /mnt/$volumeName ext4 defaults,nofail,discard 0 0" | sudo tee -a /etc/fstab
 
 printf "\e[32mFinished setting up your volume. You may now proceed if no errors were thrown.\e[39m\n"
-
