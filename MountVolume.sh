84b88037d6ed6d430b47bebf6870fe9591fc0315
GPR-ZRH <45576857+GPR-ZRH@users.noreply.github.com>
2019-07-01 02:49:58 +0200

    Update MountVolume.sh

--- MountVolume.sh
+++ MountVolume.sh
@@ -3,11 +3,11 @@
 printf "\e[31mPlease enter the name of your volume as presented on the digitalocean website -> Droplets -> Volumes\e[39m\n"
 read volumeName
 
-# Create a mount point for your volume:
+printf "Create a mount point for your volumeat /mnt/$volumeName\n"
 mkdir -p "/mnt/$volumeName"
 
 # Mount your volume at the newly-created mount point:
-mount -o "discard,defaults,noatime /dev/disk/by-id/scsi-0DO_Volume_$volumeName /mnt/$volumeName"
+mount -o discard,defaults,noatime "/dev/disk/by-id/scsi-0DO_Volume_$volumeName" "/mnt/$volumeName"
 
 # Change fstab so the volume will be mounted after a reboot
 echo "/dev/disk/by-id/scsi-0DO_Volume_$volumeName /mnt/$volumeName ext4 defaults,nofail,discard 0 0" | sudo tee -a /etc/fstab
