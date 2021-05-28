1130edd20897a2b8483993a80072222b7d3a6e36
Michael Meier <=>
2021-05-28 15:39:47 +0200

    Updated install script for Synology.. Not yet tested

--- PerforceInstall.sh
+++ PerforceInstall.sh
@@ -30,16 +30,43 @@ DownloadPerforce()
 	sudo mv p4 /usr/local/bin
 }
 
+AskSynology()
+{
+	printf "\n\e[32m Are you installing on a Synology Diskstation? (y or n)\e[39m\n"
+	while [ true ]
+	do
+		read synologyAnwser
+		if [ "$synologyAnwser" = "y" ]
+			then
+			isSynology = true
+		else 
+			isSynology = false
+			break
+		fi
+	done
+
+}
 CreateUser()
 {
-	printf "\n\e[31m\nCreating a new User called Perforce as a security measure.\nPlease provide a password for the new user!\e[39m\n"
-	sudo adduser perforce -c "Perforce User, , , "
-	sudo passwd perforce
+	if [ "$isSynology" = true ]
+	then
+		printf "\n\e[31m\nCreating a new User called perforce as a security measure.\nPlease provide a password for the new user!\e[39m\n"
+		read perforceUserPassword
+		sudo synouser --add perforce $perforceUserPassword "perforce user" 0
+	else
+		printf "\n\e[31m\nCreating a new User called perforce as a security measure.\nPlease provide a password for the new user!\e[39m\n"
+		sudo adduser perforce -c "Perforce User, , , "
+		sudo passwd perforce
+	fi
 }
 
 CreateDepot()
 {
 	printf "\n\e[31mChoose a path for the depot. Or press enter to use /p4depot as the depot root path.\e[39m"
+	if [ "$isSynology" = true ]
+	then
+		printf "\n\e[31mIf you want to be able to see the folder in your Diskstation Webinterface, the path is the volume name (ex. volume1) with /p4depot appended\e[39m"
+	fi
 	printf "\n\e[32m------------------- Input Section -------------------\e[39m\n"
 	read depotPath
 	if [ -z "$depotPath" ] || [ ${#depotPath} -lt 3 ]
@@ -69,19 +96,35 @@ CreateLog()
 SetupAutostart()
 {
 	printf "\n\e[31mCreating automatic startup script\e[39m"
-	echo "/usr/local/bin/p4d -r $depotPath -J /var/log/p4d.log" > "PerforceAutoStartServer"
-	chmod +x PerforceAutoStartServer
-	sudo mv PerforceAutoStartServer /usr/local/bin
-	printf "\n\e[31mCreating cronjob that runs the startup script on reboot\e[39m"
-	echo "@reboot root /usr/local/bin/PerforceAutoStartServer" > "PerforceCronJob"
-	chmod +x PerforceCronJob
-	sudo mv PerforceCronJob /etc/cron.d/
+#	if [ "$isSynology" = true ]
+#	then
+#		echo "/usr/local/bin/p4d -r $depotPath -J /var/log/p4d.log" > "PerforceStartServer.sh"
+#		chmod +x PerforceStartServer.sh
+#		echo "/volume1/homes/admin/p4 -p localhost:1666 admin stop" > "PerforceStopServer.sh"
+#		chmod +x PerforceStopServer.sh
+
+#	else
+		echo "/usr/local/bin/p4d -r $depotPath -J /var/log/p4d.log" > "PerforceAutoStartServer"
+		chmod +x PerforceAutoStartServer
+		sudo mv PerforceAutoStartServer /usr/local/bin
+		printf "\n\e[31mCreating cronjob that runs the startup script on reboot\e[39m"
+		echo "@reboot perforce /usr/local/bin/PerforceAutoStartServer" > "PerforceCronJob"
+		chmod +x PerforceCronJob
+		sudo mv PerforceCronJob /etc/cron.d/
+#	fi
 }
 
 Finished()
 {
 	printf "\n\e[32m\nFinished setup. Please follow the guide to finish the setup\e[39m\n"
-	printf "\e[32mYour Server will now reboot. Perforce should start automatically from now on.\e[39m\n"
+	printf "\e[32mA reboot is required for the automatic start to work. Perforce should start automatically after this restart. Do you want to restart now? (y)\e[39m\n"
+	read restartNow
+	if [ "$restartNow" = "y" ]
+	then
+		Reboot
+	else
+		printf "\n\e[32m\nMake sure to restart later!\e[39m\n"
+	fi
 }
 
 Reboot()
@@ -106,4 +149,4 @@ CreateDepot
 CreateLog
 SetupAutostart
 Finished
-Reboot
+
